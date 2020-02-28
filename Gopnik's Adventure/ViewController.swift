2d46a45db702faed86b4da81ef33400eccc2ebf3
Alex <its.alex203@gmail.com>
2020-02-28 09:11:20 -0800

    moves

--- Gopnik's Adventure/ViewController.swift
+++ Gopnik's Adventure/ViewController.swift
@@ -17,7 +17,17 @@ class ViewController: UIViewController {
     
     @IBOutlet var characterImage: UIImageView!
     
-    
+    var rightVelocityMultiplier: Double = 0.0;
+    var leftVelocityMultiplier: Double = 0.0;
+    var upVelocityMultiplier: Double = 0.0;
+    var downVelocityMultiplier: Double = 0.0;
+    var rightVelocity: Double = 1.0;
+    var leftVelocity: Double = 1.0;
+    var upVelocity: Double = 1.0;
+    var downVelocity: Double = 1.0;
+    var characterLocationX: Double = 350.0;
+    var characterLocationY: Double = 400.0;
+
     
     
     override func viewDidLoad() {
@@ -28,29 +38,51 @@ class ViewController: UIViewController {
 
     
     @objc func moveCharacter() {
+        if rightVelocityMultiplier > 0 {
+            rightVelocityMultiplier -= 0.1;
+        }
+        if leftVelocityMultiplier > 0 {
+            leftVelocityMultiplier -= 0.1;
+        }
+        if upVelocityMultiplier > 0 {
+            upVelocityMultiplier -= 0.1;
+        }
+        if downVelocityMultiplier > 0 {
+            downVelocityMultiplier -= 0.1;
+        }
+        
+        characterImage.center = CGPoint(x: characterLocationX + (rightVelocity * rightVelocityMultiplier) - (leftVelocity * leftVelocityMultiplier), y:characterLocationY + (downVelocity * downVelocityMultiplier) - (upVelocity * upVelocityMultiplier))
+        
+        characterLocationX = Double(characterImage.center.x)
+        characterLocationY = Double(characterImage.center.y)
+
        
     }
     
     @IBAction func upButtonPressed(_ sender: UILongPressGestureRecognizer) {
         if sender.state != .ended {
             NSLog("up button pressed\n")
+            upVelocityMultiplier = 2.5;
         }
     }
     
     @IBAction func rightButtonPressed(_ sender: UILongPressGestureRecognizer) {
         if sender.state != .ended {
             NSLog("right button pressed\n")
+            rightVelocityMultiplier = 2.5;
         }
     }
     
     @IBAction func downButtonPressed(_ sender: UILongPressGestureRecognizer) {
         if sender.state != .ended {
             NSLog("down button pressed\n")
+            downVelocityMultiplier = 2.5;
         }
     }
     @IBAction func leftButtonPressed(_ sender: UILongPressGestureRecognizer) {
         if sender.state != .ended {
             NSLog("left button pressed\n")
+            leftVelocityMultiplier = 2.5;
         }
     }
     
