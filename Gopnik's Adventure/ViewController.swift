e677163a9d9a94573a8910041247197eea74bc9a
alex titov <its.alex203@gmail.com>
2020-02-27 17:43:51 -0800

    outlets connected

--- Gopnik's Adventure/ViewController.swift
+++ Gopnik's Adventure/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var botomWallImage: UIImageView!
+    @IBOutlet var topWallImage: UIImageView!
+    @IBOutlet var leftWallImage: UIImageView!
+    @IBOutlet var rightWallImage: UIImageView!
+    
+    @IBOutlet var characterImage: UIImageView!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    
+    
+    @IBAction func upButtonPressed(_ sender: UIButton) {
+        
+    }
+    @IBAction func leftButtonPressed(_ sender: UIButton) {
+        
+    }
+    @IBAction func rightButtonPressed(_ sender: UIButton) {
+        
+    }
+    @IBAction func downButonPressed(_ sender: UIButton) {
+    }
+    
+    
+    
+    
 }
 
