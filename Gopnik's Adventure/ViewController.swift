8061e8e645e70946b62aa8ef7dd3e82277c905cb
alex titov <its.alex203@gmail.com>
2020-02-27 17:31:31 -0800

    Initial Commit

---
+++ Gopnik's Adventure/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gopnik's Adventure
+//
+//  Created by Test on 2/27/20.
+//  Copyright © 2020 AlexTitovProductions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
