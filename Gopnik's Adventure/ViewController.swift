4b88a8885d0811dafda80f460d5a78d2d1190990
alex titov <its.alex203@gmail.com>
2020-02-29 07:59:17 -0800

    selectors work

--- Gopnik's Adventure/ViewController.swift
+++ Gopnik's Adventure/ViewController.swift
@@ -17,6 +17,13 @@ class ViewController: UIViewController {
     
     @IBOutlet var characterImage: UIImageView!
     
+    
+    @IBOutlet var upButton: UIButton!
+    @IBOutlet var leftButton: UIButton!
+    @IBOutlet var rightButton: UIButton!
+    @IBOutlet var downButton: UIButton!
+    
+    
     var rightVelocityMultiplier: Double = 0.0;
     var leftVelocityMultiplier: Double = 0.0;
     var upVelocityMultiplier: Double = 0.0;
@@ -27,6 +34,11 @@ class ViewController: UIViewController {
     var downVelocity: Double = 1.0;
     var characterLocationX: Double = 350.0;
     var characterLocationY: Double = 400.0;
+    var upPressed: Bool = false;
+    var downPressed: Bool = false;
+    var leftPressed: Bool = false;
+    var rightPressed: Bool = false;
+
 
     
     
@@ -34,8 +46,49 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         let timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(moveCharacter), userInfo: nil, repeats: true)
-    }
+        upButton.addTarget(self, action: #selector(upButtonCalceled), for: UIControl.Event.touchCancel)
+        downButton.addTarget(self, action: #selector(downButtonCalceled), for: UIControl.Event.touchCancel)
+        leftButton.addTarget(self, action: #selector(leftButtonCalceled), for: UIControl.Event.touchCancel)
+        rightButton.addTarget(self, action: #selector(rightButtonCalceled), for: UIControl.Event.touchCancel)
+        upButton.addTarget(self, action: #selector(upButtonPressed), for: UIControl.Event.touchDown)
+        downButton.addTarget(self, action: #selector(downButtonPressed), for: UIControl.Event.touchDown)
+        leftButton.addTarget(self, action: #selector(leftButtonPressed), for: UIControl.Event.touchDown)
+        rightButton.addTarget(self, action: #selector(rightButtonPressed), for: UIControl.Event.touchDown)
 
+    }
+    
+    
+    @objc func upButtonCalceled() {
+        upPressed = false;
+    }
+    
+    @objc func downButtonCalceled() {
+        downPressed = false;
+    }
+    
+    @objc func leftButtonCalceled() {
+        leftPressed = false;
+    }
+    
+    @objc func rightButtonCalceled() {
+        rightPressed = false;
+    }
+    
+    @objc func upButtonPressed() {
+        upPressed = true;
+    }
+    
+    @objc func downButtonPressed() {
+        downPressed = true;
+    }
+    
+    @objc func leftButtonPressed() {
+        leftPressed = true;
+    }
+    
+    @objc func rightButtonPressed() {
+        rightPressed = true;
+    }
     
     @objc func moveCharacter() {
         if rightVelocityMultiplier > 0 {
@@ -51,6 +104,19 @@ class ViewController: UIViewController {
             downVelocityMultiplier -= 0.1;
         }
         
+        if rightPressed == true {
+            rightVelocityMultiplier  = 2;
+        }
+        if leftPressed == true {
+            leftVelocityMultiplier  = 2;
+        }
+        if upPressed == true {
+            upVelocityMultiplier  = 2;
+        }
+        if downPressed == true {
+            downVelocityMultiplier = 2;
+        }
+        
         characterImage.center = CGPoint(x: characterLocationX + (rightVelocity * rightVelocityMultiplier) - (leftVelocity * leftVelocityMultiplier), y:characterLocationY + (downVelocity * downVelocityMultiplier) - (upVelocity * upVelocityMultiplier))
         
         characterLocationX = Double(characterImage.center.x)
@@ -58,38 +124,6 @@ class ViewController: UIViewController {
 
        
     }
-    
-    @IBAction func upButtonPressed(_ sender: UILongPressGestureRecognizer) {
-        if sender.state != .ended {
-            NSLog("up button pressed\n")
-            upVelocityMultiplier = 2.5;
-        }
-    }
-    
-    @IBAction func rightButtonPressed(_ sender: UILongPressGestureRecognizer) {
-        if sender.state != .ended {
-            NSLog("right button pressed\n")
-            rightVelocityMultiplier = 2.5;
-        }
-    }
-    
-    @IBAction func downButtonPressed(_ sender: UILongPressGestureRecognizer) {
-        if sender.state != .ended {
-            NSLog("down button pressed\n")
-            downVelocityMultiplier = 2.5;
-        }
-    }
-    @IBAction func leftButtonPressed(_ sender: UILongPressGestureRecognizer) {
-        if sender.state != .ended {
-            NSLog("left button pressed\n")
-            leftVelocityMultiplier = 2.5;
-        }
-    }
-    
-    
-    
-    
-    
     
     
     
