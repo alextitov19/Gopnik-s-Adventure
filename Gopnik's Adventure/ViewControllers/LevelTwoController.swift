7114e06d7ee98128b3890978768f35f39c71bd80
alex titov <its.alex203@gmail.com>
2020-03-24 07:55:58 -0700

    some images added

--- Gopnik's Adventure/ViewControllers/LevelTwoController.swift
+++ Gopnik's Adventure/ViewControllers/LevelTwoController.swift
@@ -10,16 +10,15 @@ import UIKit
 
 class LevelTwoController: UIViewController {
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    //borders
+    @IBOutlet var topWallImage: UIImageView!
+    @IBOutlet var rightWallImage: UIImageView!
+    @IBOutlet var bottomWallImage: UIImageView!
+    @IBOutlet var leftWallImage: UIImageView!
+    //character
+    @IBOutlet var characterImage: UIImageView!
+    //bullets
+    @IBOutlet var bullet1Image: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
