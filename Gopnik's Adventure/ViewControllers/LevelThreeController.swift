b88f12afa1e9749e254741354799a342127eb6f4
alex titov <its.alex203@gmail.com>
2020-04-04 16:00:14 -0700

    moves and shoots

--- Gopnik's Adventure/ViewControllers/LevelThreeController.swift
+++ Gopnik's Adventure/ViewControllers/LevelThreeController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class LevelThreeController: UIViewController {
     
+    
+    @IBOutlet var topWallImage: UIImageView!
+    @IBOutlet var leftWallImage: UIImageView!
+    @IBOutlet var bottomWallImage: UIImageView!
+    @IBOutlet var rightWallImage: UIImageView!
+    
     @IBOutlet var I1BottomImage: UIImageView!
     @IBOutlet var I1TopImage: UIImageView!
     @IBOutlet var I1RightImage: UIImageView!
@@ -37,7 +43,7 @@ class LevelThreeController: UIViewController {
     @IBOutlet var I7BottomImage: UIImageView!
     @IBOutlet var I7RightImage: UIImageView!
     
-    @IBOutlet var I8Topimage: UIImageView!
+    @IBOutlet var I8TopImage: UIImageView!
     @IBOutlet var I8BottomImage: UIImageView!
     @IBOutlet var I8LeftImage: UIImageView!
     
@@ -127,29 +133,187 @@ class LevelThreeController: UIViewController {
     
     @IBOutlet var characterImage: UIImageView!
     
-    @IBOutlet var bulletImage: UIImageView!
+    @IBOutlet var bullet1Image: UIImageView!
     
     @IBOutlet var upButton: UIButton!
     
+    var rightVelocityMultiplier: Double = 0.0
+    var leftVelocityMultiplier: Double = 0.0
+    var upVelocityMultiplier: Double = 0.0
+    var downVelocityMultiplier: Double = 0.0
+    var rightVelocity: Double = 0.1
+    var leftVelocity: Double = 0.1
+    var upVelocity: Double = 0.1
+    var downVelocity: Double = 0.1
+    var characterLocationX: Double = 0.0
+    var characterLocationY: Double = 0.0
+    var downTouchesEnded: Bool = true
+    var leftTouchesEnded: Bool = true
+    var rightTouchesEnded: Bool = true
+    var inAir: Bool = false
     
+    var bulletVelocity: Double = 0.5
+    var bullet1IsInAction: Bool = false
+    var bullet1Direction: Int = 1    //left = -1, right = 1
+    var bullet1Y: Double = 600
+    var bullet1X: Double = 300
+    
+    var timer: Timer?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        timer = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: #selector(selector), userInfo: nil, repeats: true)
+        characterLocationX = Double(characterImage.center.x)
+        characterLocationY = Double(characterImage.center.y)
+
+    }
+    
+    
+    
+    @objc func selector() {
+        //character logic
+        if rightVelocityMultiplier > 0 {
+            rightVelocityMultiplier -= 0.1
+        }
+        if leftVelocityMultiplier > 0 {
+            leftVelocityMultiplier -= 0.1
+        }
+        if upVelocityMultiplier > 0 {
+            upVelocityMultiplier -= 0.01
+        }
+        if upVelocityMultiplier < 0 {
+            upVelocityMultiplier = 0
+        }
+        
+        
+        if leftTouchesEnded == false {
+            leftVelocityMultiplier = 2
+        }
+        if rightTouchesEnded == false {
+            rightVelocityMultiplier = 2
+        }
+        
+        
+        if leftBorderHitCheck(frame: characterImage.frame){
+            leftVelocityMultiplier = 0
+        }
+        if rightBorderHitCheck(frame: characterImage.frame) {
+            rightVelocityMultiplier = 0
+        }
+        if bottomBorderHitCheck(frame: characterImage.frame) {
+            upVelocityMultiplier = 0
+        }
+        if topBorderHitCheck(frame: characterImage.frame) {
+            downVelocityMultiplier = 0
+            inAir = false
+        } else {
+            downVelocityMultiplier += 0.01
+            inAir = true
+        }
+        
+        if inAir == true {
+            upButton.isUserInteractionEnabled = false
+        } else {
+            upButton.isUserInteractionEnabled = true
+        }
+        
+        
+        characterImage.center = CGPoint(x: characterLocationX + (rightVelocity * rightVelocityMultiplier) - (leftVelocity * leftVelocityMultiplier), y:characterLocationY + (downVelocity * downVelocityMultiplier) - (upVelocity * upVelocityMultiplier))
+        
+        characterLocationX = Double(characterImage.center.x)
+        characterLocationY = Double(characterImage.center.y)
+        
+        
+        if leftBorderHitCheck(frame: bullet1Image.frame) == true || rightBorderHitCheck(frame: bullet1Image.frame) == true{
+            bullet1IsInAction = false
+        }
+        
+        
+        if bullet1IsInAction == true {
+            bullet1Image.isHidden = false
+            bullet1Image.center = CGPoint(x: bullet1X + (bulletVelocity * Double(bullet1Direction)), y: bullet1Y)
+            bullet1X = Double(bullet1Image.center.x)
+            bullet1Y = Double(bullet1Image.center.y)
+        } else {
+            bullet1Image.isHidden = true
+        }
+    }
+    
+    
+    //right border hit
+    func rightBorderHitCheck(frame :CGRect) -> Bool {
+        if (frame.intersects(rightWallImage.frame) || frame.intersects(I2LeftImage.frame) || frame.intersects(I4LeftImage.frame) || frame.intersects(I6LeftImage.frame) || frame.intersects(I8LeftImage.frame) || frame.intersects(I11LeftImage.frame) || frame.intersects(I13LeftImage.frame) || frame.intersects(I15LeftImage.frame) || frame.intersects(I17LeftImage.frame) || frame.intersects(I19LeftImage.frame) || frame.intersects(I21LeftImage.frame) || frame.intersects(I24LeftImage.frame) || frame.intersects(I27LeftImage.frame) || frame.intersects(I29LeftImage.frame)) {
+            return true
+        } else {
+            return false
+        }
+    }
+    //left border hit
+    func leftBorderHitCheck(frame :CGRect) -> Bool {
+        if (frame.intersects(leftWallImage.frame) || frame.intersects(I1RightImage.frame) || frame.intersects(I3RightImage.frame) || frame.intersects(I5RightImage.frame) || frame.intersects(I7RightImage.frame) || frame.intersects(I9RightImage.frame) || frame.intersects(I10RightImage.frame) || frame.intersects(I12RightImage.frame) || frame.intersects(I14RightImage.frame) || frame.intersects(I16RightImage.frame) || frame.intersects(I18RightImage.frame) || frame.intersects(I20RightImage.frame) || frame.intersects(I22RightImage.frame) || frame.intersects(I23RightImage.frame) || frame.intersects(I25RightImage.frame) || frame.intersects(I26RightImage.frame) || frame.intersects(I28RightImage.frame)) {
+            return true
+        } else {
+            return false
+        }
+    }
+    //top border hit
+    func topBorderHitCheck(frame :CGRect) -> Bool {
+        if (frame.intersects(bottomWallImage.frame) || frame.intersects(I1TopImage.frame) || frame.intersects(I2TopImage.frame) || frame.intersects(I3TopImage.frame) || frame.intersects(I4TopImage.frame) || frame.intersects(I5TopImage.frame) || frame.intersects(I6TopImage.frame) || frame.intersects(I7TopImage.frame) || frame.intersects(I8TopImage.frame) || frame.intersects(I9TopImage.frame) || frame.intersects(I10TopImage.frame) || frame.intersects(I11TopImage.frame) || frame.intersects(I12TopImage.frame) || frame.intersects(I13TopImage.frame) || frame.intersects(I14TopImage.frame) || frame.intersects(I15TopImage.frame) || frame.intersects(I16TopImage.frame) || frame.intersects(I17TopImage.frame) || frame.intersects(I18TopImage.frame) || frame.intersects(I19TopImage.frame) || frame.intersects(I20TopImage.frame) || frame.intersects(I21TopImage.frame)) {
+            return true
+        } else {
+            return false
+        }
+    }
+    //bottom border hit
+    func bottomBorderHitCheck(frame :CGRect) -> Bool {
+        if (frame.intersects(topWallImage.frame) || frame.intersects(I1BottomImage.frame) || frame.intersects(I2BottomImage.frame) || frame.intersects(I3BottomImage.frame) || frame.intersects(I4BottomImage.frame) || frame.intersects(I5BottomImage.frame) || frame.intersects(I6BottomImage.frame) || frame.intersects(I7BottomImage.frame) || frame.intersects(I8BottomImage.frame) || frame.intersects(I9BottomImage.frame) || frame.intersects(I10BottomImage.frame) || frame.intersects(I11BottomImage.frame) || frame.intersects(I12BottomImage.frame) || frame.intersects(I13BottomImage.frame) || frame.intersects(I14BottomImage.frame) || frame.intersects(I15BottomImage.frame) || frame.intersects(I16BottomImage.frame) || frame.intersects(I17BottomImage.frame) || frame.intersects(I18BottomImage.frame) || frame.intersects(I19BottomImage.frame) || frame.intersects(I20BottomImage.frame) || frame.intersects(I21BottomImage.frame)) {
+            return true
+        } else {
+            return false
+        }
+    }
     
     
     
     @IBAction func upButtonTapped(_ sender: UIButton) {
+        upVelocityMultiplier = 5
+        
     }
     
     
     @IBAction func leftLongPress(_ sender: UILongPressGestureRecognizer) {
+        bullet1Direction = -1
+        if sender.state != .ended {
+            leftTouchesEnded = false
+        } else {
+            leftTouchesEnded = true
+        }
     }
     
     @IBAction func downLongPress(_ sender: UILongPressGestureRecognizer) {
+        if sender.state != .ended {
+            downTouchesEnded = false
+        } else {
+            downTouchesEnded = true
+        }
     }
     
     
     @IBAction func rightLongPress(_ sender: UILongPressGestureRecognizer) {
+        bullet1Direction = 1
+        if sender.state != .ended {
+            rightTouchesEnded = false
+        } else {
+            rightTouchesEnded = true
+        }
     }
     
     @IBAction func fireButtonTapped(_ sender: UIButton) {
+        bullet1IsInAction = true
+        bullet1Y = Double(characterImage.center.y)
+        bullet1X = Double(characterImage.center.x) + (bulletVelocity * Double(bullet1Direction))
+        bullet1Image.center = CGPoint(x: bullet1X + (bulletVelocity * Double(bullet1Direction)), y: bullet1Y)
     }
     
     
