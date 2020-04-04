03c672c809563a163c43a5d9d7e6e23073f34eca
alex titov <its.alex203@gmail.com>
2020-04-04 08:58:15 -0700

    connected borers

--- Gopnik's Adventure/ViewControllers/LevelThreeController.swift
+++ Gopnik's Adventure/ViewControllers/LevelThreeController.swift
@@ -9,6 +9,134 @@ import UIKit
 
 class LevelThreeController: UIViewController {
     
+    @IBOutlet var I1BottomImage: UIImageView!
+    @IBOutlet var I1TopImage: UIImageView!
+    @IBOutlet var I1RightImage: UIImageView!
+    
+    @IBOutlet var I2TopImage: UIImageView!
+    @IBOutlet var I2BottomImage: UIImageView!
+    @IBOutlet var I2LeftImage: UIImageView!
+    
+    @IBOutlet var I3TopImage: UIImageView!
+    @IBOutlet var I3BottomImage: UIImageView!
+    @IBOutlet var I3RightImage: UIImageView!
+    
+    @IBOutlet var I4TopImage: UIImageView!
+    @IBOutlet var I4LeftImage: UIImageView!
+    @IBOutlet var I4BottomImage: UIImageView!
+    
+    @IBOutlet var I5TopImage: UIImageView!
+    @IBOutlet var I5BottomImage: UIImageView!
+    @IBOutlet var I5RightImage: UIImageView!
+    
+    @IBOutlet var I6TopImage: UIImageView!
+    @IBOutlet var I6BottomImage: UIImageView!
+    @IBOutlet var I6LeftImage: UIImageView!
+    
+    @IBOutlet var I7TopImage: UIImageView!
+    @IBOutlet var I7BottomImage: UIImageView!
+    @IBOutlet var I7RightImage: UIImageView!
+    
+    @IBOutlet var I8Topimage: UIImageView!
+    @IBOutlet var I8BottomImage: UIImageView!
+    @IBOutlet var I8LeftImage: UIImageView!
+    
+    @IBOutlet var I9TopImage: UIImageView!
+    @IBOutlet var I9BottomImage: UIImageView!
+    @IBOutlet var I9RightImage: UIImageView!
+    
+    @IBOutlet var I10TopImage: UIImageView!
+    @IBOutlet var I10BottomImage: UIImageView!
+    @IBOutlet var I10RightImage: UIImageView!
+    
+    @IBOutlet var I11TopImage: UIImageView!
+    @IBOutlet var I11BottomImage: UIImageView!
+    @IBOutlet var I11LeftImage: UIImageView!
+    
+    @IBOutlet var I12TopImage: UIImageView!
+    @IBOutlet var I12BottomImage: UIImageView!
+    @IBOutlet var I12RightImage: UIImageView!
+    
+    @IBOutlet var I13TopImage: UIImageView!
+    @IBOutlet var I13BottomImage: UIImageView!
+    @IBOutlet var I13LeftImage: UIImageView!
+    
+    @IBOutlet var I14TopImage: UIView!
+    @IBOutlet var I14BottomImage: UIImageView!
+    @IBOutlet var I14RightImage: UIImageView!
+    
+    @IBOutlet var I15TopImage: UIImageView!
+    @IBOutlet var I15BottomImage: UIImageView!
+    @IBOutlet var I15LeftImage: UIImageView!
+    
+    @IBOutlet var I16TopImage: UIImageView!
+    @IBOutlet var I16BottomImage: UIImageView!
+    @IBOutlet var I16RightImage: UIImageView!
+    
+    @IBOutlet var I17TopImage: UIImageView!
+    @IBOutlet var I17BottomImage: UIImageView!
+    @IBOutlet var I17LeftImage: UIImageView!
+    
+    @IBOutlet var I18TopImage: UIImageView!
+    @IBOutlet var I18BottomImage: UIImageView!
+    @IBOutlet var I18RightImage: UIImageView!
+    
+    @IBOutlet var I19TopImage: UIImageView!
+    @IBOutlet var I19BottomImage: UIImageView!
+    @IBOutlet var I19LeftImage: UIImageView!
+    
+    @IBOutlet var I20TopImage: UIImageView!
+    @IBOutlet var I20BottomImage: UIImageView!
+    @IBOutlet var I20RightImage: UIImageView!
+    
+    @IBOutlet var I21TopImage: UIImageView!
+    @IBOutlet var I21BottomImage: UIImageView!
+    @IBOutlet var I21LeftImage: UIImageView!
+    
+    @IBOutlet var I22TopImage: UIImageView!
+    @IBOutlet var I22BottomImage: UIView!
+    @IBOutlet var I22RightImage: UIImageView!
+    
+    @IBOutlet var I23TopImage: UIImageView!
+    @IBOutlet var I23BottomImage: UIImageView!
+    @IBOutlet var I23RightImage: UIImageView!
+    
+    @IBOutlet var I24TopImage: UIImageView!
+    @IBOutlet var I24BottomImage: UIImageView!
+    @IBOutlet var I24LeftImage: UIImageView!
+    
+    @IBOutlet var I25TopImage: UIImageView!
+    @IBOutlet var I25BottomImage: UIImageView!
+    @IBOutlet var I25RightImage: UIImageView!
+    
+    @IBOutlet var I26TopImage: UIImageView!
+    @IBOutlet var I26BottomImage: UIImageView!
+    @IBOutlet var I26RightImage: UIImageView!
+    
+    @IBOutlet var I27TopImage: UIImageView!
+    @IBOutlet var I27BottomImage: UIImageView!
+    @IBOutlet var I27LeftImage: UIImageView!
+    
+    @IBOutlet var I28TopImage: UIImageView!
+    @IBOutlet var I28BottomImage: UIImageView!
+    @IBOutlet var I28RightImage: UIImageView!
+    
+    @IBOutlet var I29TopImage: UIImageView!
+    @IBOutlet var I29BottomImage: UIImageView!
+    @IBOutlet var I29LeftImage: UIImageView!
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
 }
